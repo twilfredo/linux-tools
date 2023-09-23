95a4cc4fb14b48be124bf8b942924c5fde377853
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2023-09-23 11:11:47 +1000

    display-setup: add dynamic audio sinkid detection
    Using `wpctl` parse the output sinks by the output dev name to get the
    sink id. These cannot be hardcoded, as the list will change as other
    devices are added/removed.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- display-setup.sh
+++ display-setup.sh
@@ -7,15 +7,42 @@ secondary="DP-3"
 secondary_cfg="2560x1440@143.912"
 tv="HDMI-1"
 tv_cfg="3840x2160@120.000"
+# Audio outputs by name
+logitech_speaker="Starship/Matisse HD Audio Controller Analog Stereo"
+tv_soundbar="Navi 21/23 HDMI/DP Audio Controller Digital Stereo (HDMI 4)"
+
+get_wpctl_sink_id_logitech_speaker() {
+    wpctl status |  awk '/Sinks:/{flag=1; next} /Sink endpoints:/{flag=0} flag' | grep "$logitech_speaker" | grep -o '[0-9][0-9]\+\.' | awk -F '.' '{print $1}'
+}
+
+get_wpctl_sink_id_tv() {
+    wpctl status | awk '/Sinks:/{flag=1; next} /Sink endpoints:/{flag=0} flag' | grep "$tv_soundbar" | grep -o '[0-9][0-9]\+\.' | awk -F '.' '{print $1}'
+}
 
 set_workstation() {
    # Secondary positioned at [0, 0] far left.
    # Primary positioned at [1440, 270] respective to [0, 0] of secondary.
    gnome-monitor-config set -LM "$secondary" -m "$secondary_cfg" -t left -LpM "$primary" -m "$primary_cfg" -x 1440 -y 270 -t normal
+   sleep 1
+   #re-route audio sink
+   sink_id=$(get_wpctl_sink_id_logitech_speaker)
+   if [ -z "$sink_id" ]; then
+       echo "Error: sink not detected for $logitech_speaker"
+       return
+   fi
+   wpctl set-default $sink_id
 }
 
 set_media() {
    gnome-monitor-config set -LpM "$tv" -t normal -m "$tv_cfg" -s 2
+   sleep 1
+   #re-route audio sink
+   sink_id=$(get_wpctl_sink_id_tv)
+   if [ -z "$sink_id" ]; then
+       echo "Error: sink not detected for $tv_soundbar"
+       return
+   fi
+   wpctl set-default $sink_id
 }
 
 launch_steam() {
