ca50caf7b78a6510099da84827e4b6393fd57cac
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2024-12-20 17:36:40 +1000

    kde-display-setup: update to match new cfg
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- kde-display-setup.sh
+++ kde-display-setup.sh
@@ -2,15 +2,15 @@
 # Switch between workstation monitor setup and gaming setup this depends on KDE and kscreen-tools
 
 primary="DP-1"
-# The monitor_mode is extracted from `kscreen-doctor --outputs
-primary_mode="2"
+# The monitor_mode is extracted from `kscreen-doctor --outputs`
+primary_mode="60"
 secondary="DP-3"
-secondary_mode="1"
+secondary_mode="2"
 tv="HDMI-A-1"
-tv_mode="11"
+tv_mode="103"
 # Audio outputs by name
 logitech_speaker="Starship/Matisse HD Audio Controller Analog Stereo"
-tv_soundbar="Navi 21/23 HDMI/DP Audio Controller Digital Stereo (HDMI 4)"
+tv_soundbar="Navi 21/23 HDMI/DP Audio Controller Digital Stereo"
 
 get_wpctl_sink_id_logitech_speaker() {
     wpctl status |  awk '/Sinks:/{flag=1; next} /Sink endpoints:/{flag=0} flag' | grep "$logitech_speaker" | grep -o '[0-9][0-9]\+\.' | awk -F '.' '{print $1}'
@@ -24,11 +24,13 @@ set_workstation() {
    # Secondary positioned at [0, 0] far left.
    # Primary positioned at [1440, 270] respective to [0, 0] of secondary.
    kscreen-doctor output.$primary.enable \
+		  output.$primary.vrrpolicy.never \
                   output.$primary.priority.0 \
                   output.$primary.mode.$primary_mode \
                   output.$primary.position.1440,270 \
                   output.$primary.vrrpolicy.automatic \
                   output.$secondary.enable \
+                  output.$secondary.vrrpolicy.never \
                   output.$secondary.mode.$secondary_mode \
                   output.$secondary.position.0,0 \
                   output.$secondary rotation.2 \
@@ -47,6 +49,7 @@ set_workstation() {
 set_media() {
    kscreen-doctor output.$tv.enable \
                   output.$tv.mode.$tv_mode \
+                  output.$tv.vrrpolicy.never \
                   output.$tv.priority.0 \
                   output.$tv.scale.2 \
                   output.$tv.position.0,0 \
@@ -110,4 +113,4 @@ elif [ "$1" = "game" ]; then
 else
   echo "Invalid argument"
   exit 1
-fi 
+fi
